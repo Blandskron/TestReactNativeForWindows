c50868de10095d7628bbce49027ff0272b8d5a94
blandskron <bastianlandskronfreelancer@gmail.com>
2024-10-30 11:12:39 -0300

    update

---
+++ windows/TestComponents/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::TestComponents::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::TestComponents::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
